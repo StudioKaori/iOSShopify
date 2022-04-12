8e3cd3ceaf8e3646b88a0249872714d97ae4ca60
Studio Kaori <studiokaori.com@gmail.com>
2022-04-12 21:59:16 +0200

    Test: fetch products information in main view controller

--- Shopify/ViewController.swift
+++ Shopify/ViewController.swift
@@ -13,10 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // API Key
-                if let APIKEY = KeyManager().getValue(key: "shopDomain") as? String {
-                    print(APIKEY)
-                }
+        // Shopify storefront API doc: See 'Object' not 'Query' https://shopify.dev/api/storefront/2022-04/objects/Shop#fields
+
         let client = Graph.Client(
             shopDomain: KeyManager().getValue(key: "shopDomain") as! String,
             apiKey:     KeyManager().getValue(key: "apiKey") as! String
@@ -27,7 +25,7 @@ class ViewController: UIViewController {
         let query = Storefront.buildQuery { $0
             .shop { $0
                 .name()
-                //.currencyCode()
+                .moneyFormat()
                 .refundPolicy { $0
                     .title()
                     .url()
@@ -38,13 +36,33 @@ class ViewController: UIViewController {
         let task = client.queryGraphWith(query) { response, error in
             let name         = response?.shop.name
             //let currencyCode = response?.shop.currencyCode
-            //let moneyFormat  = response?.shop.moneyFormat
+            let moneyFormat  = response?.shop.moneyFormat
             
             print(name)
-            //print(moneyFormat)
+            print(moneyFormat)
         }
         task.resume()
         // End of shop information
+        
+        // products information
+        let productsQuery = Storefront.buildQuery { $0
+            .products(first: 3) { $0
+                .edges { $0
+                .node { $0
+                    .id()
+                    .title()
+                }
+                }
+            }
+        }
+
+        let productsTask = client.queryGraphWith(productsQuery) { response, error in
+            let response         = response
+
+            print(response)
+        }
+        productsTask.resume()
+        // End of products information
         
         
     }
