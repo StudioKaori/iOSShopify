8b4f15dd850c281386b6e31679e7727e6f4b2f61
Studio Kaori <studiokaori.com@gmail.com>
2022-04-12 21:41:06 +0200

    Test: Get Shopify shop information in main view controller

--- Shopify/ViewController.swift
+++ Shopify/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Buy
 
 class ViewController: UIViewController {
 
@@ -16,6 +17,36 @@ class ViewController: UIViewController {
                 if let APIKEY = KeyManager().getValue(key: "shopDomain") as? String {
                     print(APIKEY)
                 }
+        let client = Graph.Client(
+            shopDomain: KeyManager().getValue(key: "shopDomain") as! String,
+            apiKey:     KeyManager().getValue(key: "apiKey") as! String
+        )
+        
+ 
+        // Shop information
+        let query = Storefront.buildQuery { $0
+            .shop { $0
+                .name()
+                //.currencyCode()
+                .refundPolicy { $0
+                    .title()
+                    .url()
+                }
+            }
+        }
+
+        let task = client.queryGraphWith(query) { response, error in
+            let name         = response?.shop.name
+            //let currencyCode = response?.shop.currencyCode
+            //let moneyFormat  = response?.shop.moneyFormat
+            
+            print(name)
+            //print(moneyFormat)
+        }
+        task.resume()
+        // End of shop information
+        
+        
     }
 
 
