e43a54dd28e6c1229f72f5e0f5cdca62911a1f55
Studio Kaori <studiokaori.com@gmail.com>
2022-04-18 07:18:34 +0200

    Add: Alert options to image picker

--- Shopify/ViewController/FittingRoomViewController.swift
+++ Shopify/ViewController/FittingRoomViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FittingRoomViewController: UIViewController, UIImagePickerController, StampSelectViewControllerDelegate {
+class FittingRoomViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerController, StampSelectViewControllerDelegate {
     
     @IBOutlet var stampBaseView: StampBaseView!
 
@@ -37,6 +37,40 @@ class FittingRoomViewController: UIViewController, UIImagePickerController, Stam
     
     @IBAction func saveTapped(){
         
+    }
+    
+    func pickImage(sourceType: UIImagePickerController.SourceType) {
+        // check if the source is available
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func showSourceSelection() {
+        // create alert
+        let alert = UIAlertController(title: "Select", message: "Please select outfits", preferredStyle: .alert)
+        
+        // alert options
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action: UIAlertAction!) in
+            self.pickImage(sourceType: .camera)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Photo Library", style: .default) { (Action: UIAlertAction) in
+            self.pickImage(sourceType: .photoLibrary)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action: UIAlertAction) in
+            print("Picker canceled")
+        }
+        
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
 
