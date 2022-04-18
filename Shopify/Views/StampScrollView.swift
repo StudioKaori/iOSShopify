64a61051b9ca7656b66d6906ef2b1874123b57b7
Studio Kaori <studiokaori.com@gmail.com>
2022-04-18 18:11:41 +0200

    Add StampScrollView to the stamp to be zoomed in

---
+++ Shopify/Views/StampScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  StampScrollView.swift
+//  Shopify
+//
+//  Created by Kaori Persson on 2022-04-18.
+//
+
+import UIKit
+
+class StampScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
