3bdb4ef2c473310b3357562f2ac15e90c889f4cf
Studio Kaori <studiokaori.com@gmail.com>
2022-04-18 06:54:56 +0200

    Add: FittingRoomVC

---
+++ Shopify/ViewController/FittingRoomViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FittingRoomViewController.swift
+//  Shopify
+//
+//  Created by Kaori Persson on 2022-04-18.
+//
+
+import UIKit
+
+class FittingRoomViewController: UIViewController, UIImagePickerController, StampSelectViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
