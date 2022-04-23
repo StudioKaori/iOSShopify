ca421804546ac95c95749aa8c78e801b0dc0ece2
Studio Kaori <studiokaori.com@gmail.com>
2022-04-23 14:54:58 +0200

    Add: constraints to stamp image view to give some margin

--- Shopify/Layout/TilesCollectionViewFlowLayout.swift
+++ Shopify/Layout/TilesCollectionViewFlowLayout.swift
@@ -8,10 +8,6 @@
 import UIKit
 
 class TilesCollectionViewFlowLayout: UICollectionViewFlowLayout {
-
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
     
     @IBInspectable var numberOfColumns: Int = 4 {
         didSet {
@@ -33,7 +29,7 @@ class TilesCollectionViewFlowLayout: UICollectionViewFlowLayout {
         let columns = CGFloat(isWide ? numberOfColumns : numberOfColumns / 2)
         let margins = collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + sectionInset.left + sectionInset.right
         let spacings = minimumLineSpacing * (columns - 1)
-        let width = (collectionView.bounds.width - margins - spacings - 5) / columns
+        let width = (collectionView.bounds.width - margins - spacings - 10) / columns
         let height = width * heightRatio
         itemSize = CGSize(width: width, height: height)
     }
