1e381167455de0878e5f10ca54953383ab6404f7
Studio Kaori <studiokaori.com@gmail.com>
2022-05-10 16:11:08 +0200

    Add: struct Hero

--- Shopify/ViewController/ViewController.swift
+++ Shopify/ViewController/ViewController.swift
@@ -32,6 +32,23 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     private let scrollHight: CGFloat = UIScreen.main.bounds.height
     private let imageWidth: CGFloat = UIScreen.main.bounds.width
     
+    
+    // hero information
+    struct Hero{
+        var title: String
+        var image: UIImage
+        var buttonLabel: String
+        var nextViewController: AnyObject
+        var nextStoryBoardId: String
+    }
+    
+    var hero1: Hero = Hero(
+        title: "Make Up Inspiration", image: <#T##UIImage#>, buttonLabel: <#T##String#>, nextViewController: <#T##AnyObject#>, nextStoryBoardId: <#T##String#>
+    )
+    
+    var heros: [Hero] = []
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
